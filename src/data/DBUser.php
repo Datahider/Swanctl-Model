470771b5ddecba49e60b88b8bc648256e8aa9443
drweb <drweb@DESKTOP-6245KLE>
2023-10-08 21:31:31 +0300

    added field integration_id to DBUser

--- src/data/DBUser.php
+++ src/data/DBUser.php
@@ -20,7 +20,9 @@ class DBUser extends \losthost\DB\DBObject {
                 id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 login varchar(50) NOT NULL,
                 password varchar(50) NOT NULL,
-                PRIMARY KEY (id)
+                integration_id bigint(20),
+                PRIMARY KEY (id),
+                UNIQUE INDEX INTEGRATION_ID(integration_id)
             ) COMMENT = 'v1.0.0'
             END;
     
